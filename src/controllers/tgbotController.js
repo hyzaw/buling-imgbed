2a70400fcb4600408398229675983f4b63ff5dc1
hyzaw <62041055+hyzaw@users.noreply.github.com>
2024-11-18 01:22:32 +0800

    Update tgbotController.js

--- src/controllers/tgbotController.js
+++ src/controllers/tgbotController.js
@@ -54,6 +54,7 @@ export const tgbotController = {
 
             // 获取文件并上传
             const fileInfo = await getFileInfo(c.env.TG_BOT_TOKEN, fileId);
+            console.log(fileInfo);
             const fileUrl = `https://api.telegram.org/file/bot${c.env.TG_BOT_TOKEN}/${fileInfo.file_path}`;
 
             const response = await fetch(fileUrl);
